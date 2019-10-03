5f6b172b7b02a3f5055a6edd1f78122f0ae2e75b
Emma <emma.diziere@gmail.com>
2019-10-03 11:08:50 +0200

    kyf

--- views/back/auth.php
+++ views/back/auth.php
@@ -1,24 +1,62 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" href="CSS/style.css" />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title> Projet Alternant </title>
+  </head>
 
-$login_valide = "admin";
-$pwd_valide = "admin";
+      <body>
+          <div id="container">
+              <form>
+                <?php
+session_start();
+if(isset($_POST['username']) && isset($_POST['password']))
+{
+    // connexion à la base de données
+    $db_username = 'root';
+    $db_password = '';
+    $db_name     = 'tp_1';
+    $db_host     = 'localhost';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die('could not connect to database');
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    if ($login_valide == $_POST['username'] && $pwd_valide == $_POST['password']) {
+    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+    // pour éliminer toute attaque de type injection SQL et XSS
+    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username']));
+    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
 
-        session_start ();
-
-        $_SESSION['username'] = $_POST['username'];
-        $_SESSION['password'] = $_POST['password'];
-
-        header ('location: page_formateur.php');
-    }
-    else {
-        echo '<body onLoad="alert(\'Membre non reconnu...\')">';
-        header ('location: page_apprenti.php');
+    if($username !== "" && $password !== "")
+        {
+          $requete = "SELECT count(*) FROM apprenti where
+          login = '".$username."' and password = '".$password."' ";
+          $exec_requete = mysqli_query($db,$requete);
+          $reponse      = mysqli_fetch_array($exec_requete);
+          $count = $reponse['count(*)'];
+          if($count!=0) // nom d'utilisateur et mot de passe correctes
+          {
+             $_SESSION['username'] = $login;
+             header('Location: apprenti.php');
+          }
+          else
+          {
+             header('Location: index.php?erreur=1'); // utilisateur ou mot de passe incorrect
+          }
+        }
+    else
+    {
+       header('Location: index.php?erreur=2'); // utilisateur ou mot de passe vide
     }
 }
-else {
-    echo 'Les variables du formulaire ne sont pas déclarées.';
+else
+{
+   header('Location: index.php');
 }
-iss
+mysqli_close($db); // fermer la connexion
+?>
+              </form>
+          </div>
+      </body>
+  </html>
--- views/front/index.php
+++ views/front/index.php
@@ -2,33 +2,34 @@
 <html>
   <head>
     <meta charset="utf-8">
-      <link rel="stylesheet" href="../../css/style.css" />
-      <link rel="sytlesheet" href="../../css/geoffrey.css" />
+      <link rel="stylesheet" href="CSS/style.css" />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title> Projet Alternant </title>
   </head>
 
-  <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">Deconnexion</a>
-      <a class="navbar-brand" href="#">TP</a>
-  </nav>
 
+      <body>
+          <div id="container">
+              <form action="verification.php" method="POST">
+                  <h1>Connexion</h1>
 
-  <body>
-  <div class="login-front">
-      <form class="sign-in-htm" action="./api/user/login.php" method="POST">
-          <div class="group">
-              <label for="user" class="label">Identifiant</label>
-              <input id="username" name="username" type="text" class="input">
+                  <label><b>Nom d'utilisateur</b></label>
+                  <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+                  <label><b>Mot de passe</b></label>
+                  <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+                  <input type="submit" id='submit' value='LOGIN' >
+                  <?php
+                  if(isset($_GET['erreur']))
+                  {
+                      $err = $_GET['erreur'];
+                      if($err==1 || $err==2)
+                          echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                  }
+                  ?>
+              </form>
           </div>
-          <div class="group">
-              <label for="pass" class="label">Mot de Passe</label>
-              <input id="password" name="password" type="password" class="input" data-type="password">
-          </div>
-          <div class="group">
-              <input type="submit" class="button" value="Sign In">
-          </div>
-          <div class="hr"></div>
-      </form>
-  </div>
-  </body>
-</html>
+      </body>
+  </html>
